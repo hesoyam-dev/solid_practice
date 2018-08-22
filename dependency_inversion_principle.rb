63247d4bb9c3b9baa87a7ac5db060d06334dbd70
Oleh Budurovych <oleh_budurovych@epam.com>
2018-08-22 10:41:28 +0300

    add change to dependency inversion code example

--- dependency_inversion_principle.rb
+++ dependency_inversion_principle.rb
@@ -1,5 +1,53 @@
+# Bad example
+
 class Developer < ProjectTeam
-  include TestBase
+  attr_accessor :programming_language
+
+  def initialize(programming_language)
+    @programming_language = programming_language
+  end
+
+  def develop
+    sprint.times do
+      puts "Writing code using #{@programming_language}"
+    end
+  end
+
+  def write_unit_tests
+    UnitTests.new.write
+  end
+
+  def write_integration_tests
+    IntegrationTests.new.write
+  end
+
+  def write_functional_tests
+    FunctionalTests.new.write
+  end
+end
+
+class UnitTests
+  def write
+    # Implementation for unit tests writing
+  end
+end
+
+class IntegrationTests
+  def write
+    # Implementation for integration tests writing
+  end
+end
+
+class FunctionalTests
+  def write
+    # Implementation for functional tests writing
+  end
+end
+
+
+# Good example
+
+class Developer < ProjectTeam
   attr_accessor :programming_language
 
   def initialize(programming_language)
