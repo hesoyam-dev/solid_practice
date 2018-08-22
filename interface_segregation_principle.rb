095f217eaf48205180f729fb40791dc8a165ef96
Oleh Budurovych <oleh_budurovych@epam.com>
2018-08-22 10:33:23 +0300

    first commit

--- interface_segregation_principle.rb
+++ interface_segregation_principle.rb
@@ -1,3 +1,23 @@
+# Bad example
+
+class ProjectA
+  PROJECT_VERSION = '0.1.0'.freeze
+
+  def release
+    @project_team.prepare_release(PROJECT_VERSION)
+  end
+end
+
+class ProjectB
+  PROJECT_VERSION = '0.1.2'.freeze
+
+  def release
+    @project_team.prepare_release(PROJECT_VERSION)
+  end
+end
+
+# Good example
+
 class DeliveryManager
   def prepare_release(version)
     "Release #{version}"
@@ -14,7 +34,7 @@ end
 
 class ProjectB
   PROJECT_VERSION = '0.1.2'.freeze
-  
+
   def release
     @dm.prepare_release(PROJECT_VERSION)
   end
